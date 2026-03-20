ad86ad397601d034b6120e27a3cfec2146236317
Son Tran <davidtran.forwork@gmail.com>
2026-03-19 20:46:30 -0400

    Upgrade the weight

--- son_main.cpp
+++ son_main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 //but bad in other seasons, he is still likely to be picked
 int main(){
     vector<Player> all_players = load_players("player_per_game.csv");
-    build_different_team(all_players, "OKC");
+    build_different_team(all_players, "LAL");
     //cout << "Loaded " << all_players.size() << " data rows" << endl;
     //original data has 33280 rows, but after loading we have 25960 rows, so it was filtered by the constraint at least 10 minutes per game, and rows missing columns (Jack)
     return 0;
