1d9dc84ec86e36506cf7ba8251053c1735b1b429
LTK107 <alexisjovon@gmail.com>
2026-03-25 17:02:28 -0400

    maybe we're almost there?

--- SplayTree.cpp
+++ SplayTree.cpp
@@ -4,348 +4,101 @@
 #include <iostream>
 #include <vector>
 #include "classes_and_functions.h"
+#include <unordered_map>
+#include "SplayTree.h"
+
+
 using namespace std;
 
+//this frucntion returns a teamResult, which is a collection of the best 5 players for that team
+    TeamResult getTheTeam(const vector<Player> &all_players, string team_code) {
 
+    unordered_map<string, Player> average_players;//Key is name, value is the actual Player object
+    unordered_map<string, unordered_map<string, int>> position_counts; //Get this map to keep track of the most played positions for a player career
+    //It is needed because for example, Kobe Bryant played mostly as a SG, but his last entries (last season) was a PF and our data picked up PF
+    for (const auto& p : all_players) {
+        if (p.team !=team_code) {
+            continue;
+        }
+        if (average_players.find(p.name) == average_players.end()) {
+            average_players[p.name] = p;//First time got the player => Insert them
+        }else {//If see the players for 2+ times, take the sum of every stats
+            average_players[p.name].ppg += p.ppg;
+            average_players[p.name].apg += p.apg;
+            average_players[p.name].rpg += p.rpg;
+            average_players[p.name].fg_pct += p.fg_pct;
+            average_players[p.name].three_pm_pg += p.three_pm_pg;
+            average_players[p.name].bpg += p.bpg;
+            average_players[p.name].spg += p.spg;
+            average_players[p.name].season_count++;
+        }
+        position_counts[p.name][p.position]++; //We will take the most frequent positions
+    }
+    for (auto& pair : average_players) {
+        string key_name = pair.first;
+        Player& value_player = pair.second;
 
-//Create a Splay tree class
-struct Node {
-    //initalize the Node's attributes
-    //FOR NOW, USE A BASIC DATA TO COMPARE
-    Node* left;
-    Node* right;
-    Node* parent;
+        value_player.ppg /= value_player.season_count;
+        value_player.apg /= value_player.season_count;
+        value_player.rpg /= value_player.season_count;
+        value_player.fg_pct /= value_player.season_count;
+        value_player.three_pm_pg /= value_player.season_count;
+        value_player.spg /= value_player.season_count;
+        value_player.bpg /= value_player.season_count;
 
-    //each node will store a player object
-    Player thePlayer;
-    float player_grade = thePlayer.grade;
-    //create function for the Node
-    //constructor
-    Node(Player& thisPlayer, Node* theParent) {
-        //makeing testNum equal to the grade of the player, to making comparing easier
-        thePlayer = thisPlayer;
-        left = nullptr;
-        right = nullptr;
-        parent = theParent;
+        string best_position = "";//Keep the position with most counts
+        int best_count = 0;
+
+        for (auto& position_pair : position_counts[value_player.name]) {
+            if (position_pair.second > best_count) {
+                best_count = position_pair.second;
+                best_position = position_pair.first;
+            }
+        }
+        value_player.position = best_position;//Reset the position
+        compute_grade(value_player);//Call compute grade again because the grades calculated in load_data is not equivalent any more
+    }
+    for (auto& pair: average_players) {
+        Player& p = pair.second;
+        if (p.name == "Dwyane Wade") {
+            cout << p.name << "team " << p.team << "position " << p.position << "Grades: " << p.grade<< endl;
+        }
     }
 
-};
-struct SplayTree {
 
-    //the splay tree will be composed of nodes, so it's needs a nested Node struct
+    SplayTree sgTree, pgTree, sfTree, pfTree, cTree;
 
-
-    //the only attriubte the splay tree has is the root
-    Node* theRoot;
-
-
-
-    //need logic for splaying nodes up to the root
-    //either we need to zig, zig zig, or zig zag
-
-    //we use zig to rotate the Node up
-    Node* Zig(Node* theNode) {
-        Node* oldParent = theNode->parent;
-        //see if current node is a left or right kid
-        if (oldParent->left == theNode) {
-            //it's on the left, so rotate right
-
-            //we have to rotate the pointer (similar to AVL rotate)
-            Node* grandparent = oldParent->parent;
-            Node* grandchild = theNode->right;
-            oldParent->left = grandchild;
-
-            //update the parent if necssaary
-            if (grandchild != nullptr) {
-                grandchild->parent = oldParent;
-            }
-
-
-            //update the parent pointers
-            theNode->right = oldParent;
-            oldParent->parent = theNode;
-
-            theNode->parent = grandparent;
-
-            //check if the current node is now the root
-            if (grandparent == nullptr) {
-                theRoot = theNode;
-            }
-            else {
-                //this means the grandparent wasn't the root,
-                //and we have to make sure the tree stays connected
-                //check which sid the old parent was on
-                if (grandparent->left == oldParent) {
-                    //attach it to the coorect side of the tree
-                    grandparent->left = theNode;
+        for (auto& pair : average_players){
+            Player& p = pair.second;
+            if (p.team == team_code){
+                if (p.position == "PG"){
+                    pgTree.insert(p);
                 }
-                else
-                    grandparent->right = theNode;
-
-            }
-
-
-        }
-        else {
-            //it's on the right, so rotate left
-
-            //we have to rotate the pointer (similar to AVL rotate)
-            Node* grandparent = oldParent->parent;
-            Node* grandchild = theNode->left;
-            oldParent->right = grandchild;
-
-            //update the parent if necssaary
-            if (grandchild != nullptr) {
-                grandchild->parent = oldParent;
-            }
-
-            //update the parent pointers
-            theNode->left = oldParent;
-            oldParent->parent = theNode;
-
-            theNode->parent = grandparent;
-
-            //check if the current node is now the root
-            if (grandparent == nullptr) {
-                theRoot = theNode;
-            }
-            else {
-                //this means the grandparent wasn't the root,
-                //and we have to make sure the tree stays connected
-                //check which sid the old parent was on
-                if (grandparent->right == oldParent) {
-                    //attach it to the coorect side of the tree
-                    grandparent->right = theNode;
+                else if (p.position == "SG"){
+                    sgTree.insert(p);
                 }
-                else
-                    grandparent->left = theNode;
-
-            }
-        }
-        return theNode;
-
-    }
-
-    //we uses zig zag to rotate the the node up, as well as its parent up
-    //so the node ends up being th new parent of old parent, and grandparent is the child of old parent
-    Node* ZigZig(Node* theNode) {
-
-        Node* oldParent = theNode->parent;
-        Node* grandparent = oldParent->parent;
-
-        //check if there's a grandparent, it not we can't do muitplie rotations
-        if (grandparent == nullptr) {
-            return theNode;
-        }
-
-        // we need to zig the old parent first, then zig the current node
-        Zig(oldParent);
-        Zig(theNode);
-
-        return theNode;
-
-    }
-
-    //whne the parent and curr node on opposite sides, we're gonna rotate the curent
-    //node up twice
-    Node* ZigZag(Node* theNode) {
-
-        Node* oldParent = theNode->parent;
-        Node* grandparent = oldParent->parent;
-
-        //check if there's a grandparent, it not we can't do muitplie rotations
-        if (grandparent == nullptr) {
-            return theNode;
-        }
-
-        //we're gonna zig up the current node twice. once with its parent, then again with the og grandparent
-        Zig(theNode);
-        Zig(theNode);
-
-        return theNode;
-    }
-
-
-
-    //splay  function
-    Node* Splay(Node* theNode) {
-
-        while (theNode != theRoot) {
-
-            //check if the node has grandparent, if not do a zig
-            if (theNode->parent->parent == nullptr) {
-                //USE A ZIG
-                Zig(theNode);
-            }
-            //now we have to chekc if the nodes are on the same side
-            //we use the parent pointers to work around the tree and check
-            //if true, we needs a zig zig
-            else if ((theNode->parent->left == theNode && theNode->parent->parent->left == theNode->parent) || (theNode->parent->right == theNode && theNode->parent->parent->right == theNode->parent)) {
-                //USE A ZIG ZIG
-                ZigZig(theNode);
-            }
-            //if not on the same side, we need a zig zag
-            else {
-                //USE A ZAG
-                ZigZag(theNode);
-            }
-
-        }
-        return theNode;
-
-    }
-
-    //for this project, we will only need the insert and search methods
-    //all of these are the same as a BST, but must splay accordiningly
-
-    //create a helper for insert
-    Node* inserHelper(Node* root, Player& thePlayer) {
-
-        if (root == nullptr) {
-            return new Node(thePlayer, nullptr);
-            //return temp;
-        }
-        if (thePlayer.grade < root->player_grade) {
-            if (root->left == nullptr) {
-                root->left = new Node(thePlayer, root);
-                return root->left;
-            }
-            return inserHelper(root->left, thePlayer);
-        }
-        else {
-            if (root->right == nullptr) {
-                root->right = new Node(thePlayer, root);
-                return root->right;
+                else if (p.position == "SF"){
+                    sfTree.insert(p);
+                }
+                else if (p.position == "PF"){
+                    pfTree.insert(p);
+                }
+                else if (p.position == "C"){
+                    cTree.insert(p);
+                }
             }
         }
 
-        return inserHelper(root->right, thePlayer);
+        TeamResult result;
 
-    }
+        result.pg=pgTree.findHighestGrade();
+        result.sg=sgTree.findHighestGrade();
+        result.sf=sfTree.findHighestGrade();
+        result.pf=pfTree.findHighestGrade();
+        result.c=cTree.findHighestGrade();
 
-    //insert
-    void insert(Player& currentPlayer) {
-
-        //call the helper
-        Node* temp = inserHelper(theRoot, currentPlayer);
-
-        //splay the node adn assign it to the root
-        theRoot = Splay(temp);
-
-    }
-
-
-
-
-
-
-
-    //searches for the 5 diff positions
-
-    //search for a splay tree is the same logic as a BST, just with splaying at the end
-    Node* search(Node* root, int num) {
-
-        //if the tree is empty
-        if (root == nullptr) {
-            return nullptr;
-        }
-        //if we found the id
-        else if (num == root->player_grade) {
-            //since we found it, splay it up in the tree
-            Splay(root);
-            //now return the node
-            return root;
-        }
-        //if the id is less than
-        else if (num < root->player_grade) {
-            return search(root->left, num);
-        }
-        //if the id is greater than
-        else {
-            return search(root->right, num);
-        }
-    }
-
-    //helper functions for the find functions
-    Node* findNum1(Node* theNode, Node* tempNode) {
-       //we need to go through each node in the tree, so use traversal logic
-
-        if (theNode == nullptr) {
-            return tempNode;
-        }
-        else {
-            tempNode = findNum1(theNode->left, tempNode);
-            tempNode = findNum1(theNode->right, tempNode);
-            //now check if the current node's value is greater than the tmep node's
-            //if true, update temp
-            if (theNode->player_grade > tempNode->player_grade) {
-                tempNode = theNode;
-            }
-        }
-
-        return tempNode;
+        cout << "Splay Tree Check";
+        return result;
 
 
     }
-
-
-    //the find functions will be used to find and retunr the node with the highest values
-    //in our case, the best player per position
-    Node* getNum1() {
-
-        //create a temp node
-        Node* temp = theRoot;
-
-        //call the helper
-        temp = findNum1(theRoot, temp);
-
-        return temp;
-
-     }
-
-
-    //postorder traversal helper function for the destructor
-    void postOrderHelper(Node* theNode) {
-
-        if (theNode == nullptr) {
-            return;
-        }
-        else {
-            postOrderHelper(theNode->left);
-            postOrderHelper(theNode->right);
-            delete theNode;
-        }
-
-    }
-
-
-    //Destructor for the tree, using post order traveslar, as we need to go through the dree and delte each node
-    ~SplayTree() {
-
-        //call the helper function
-        postOrderHelper(theRoot);
-        theRoot = nullptr;
-    }
-
-
-
-    public:
-    //create a function to add the players to the tree, using the insert function
-    void playersIntoTree(vector<Player>& players) {
-
-        //use a for loop to insert each player into a node for the tree
-        for (auto p : players) {
-            insert(p);
-        }
-
-    }
-
-    //this frucntion returns a teamResult, which is a collection of the best 5 players for that team
-    //TeamResult getTheTeam() {}
-
-
-
-
-
-
-};
--- jovon_main.cpp
+++ jovon_main.cpp
@@ -1,50 +1,15 @@
 //
 // Created by cladu on 3/25/2026.
 //
-#include "SplayTree.cpp"
+#include "SplayTree.h"
 #include "classes_and_functions.h"
 #include <string>
+
 using namespace std;
 int main() {
-    SplayTree sg_tree;
 
+    vector<Player> players = load_players("player_per_game.csv");
 
-    Player jordan;
-    jordan.name = "Michael Jordan";
-    jordan.team = "Bulls";
-    jordan.position = "SG";
-    jordan.ppg = 5.0f;
-    jordan.apg = 5.0f;
-    jordan.rpg = 5.0f;
-    jordan.fg_pct = 5.0f;
-    jordan.three_pm_pg = 5.0f;
-    jordan.bpg = 5.0f;
-    jordan.spg = 5.0f;
-
-    jordan.grade = 5.0f;
-
-    Player lebron;
-    lebron.name = "lebron";
-    lebron.team = "LAL";
-    lebron.position = "SG";
-    lebron.ppg = 5.0f;
-    lebron.apg = 5.0f;
-    lebron.rpg = 5.0f;
-    lebron.fg_pct = 5.0f;
-    lebron.three_pm_pg = 5.0f;
-    lebron.bpg = 5.0f;
-    lebron.spg = 5.0f;
-
-    lebron.grade = 6.0f;
-
-    vector<Player> players;
-    players.push_back(jordan);
-    players.push_back(lebron);
-
-    sg_tree.playersIntoTree(players);
-
-    Node* node = sg_tree.search(sg_tree.theRoot, 5.0);
-    cout<<(node->player_grade)<<endl;
-
+    getTheTeam(players, "LAL");
     return 0;
 }
